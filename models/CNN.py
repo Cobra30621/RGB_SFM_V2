ad5a040ed933578c40456b9d80e3aa0c867058fe
JIAN-MING <tonyvony0821@gmail.com>
2024-03-16 21:21:14 +0800

    update CNN to make it more like SFMCNN

--- models/CNN.py
+++ models/CNN.py
@@ -4,21 +4,21 @@ class CNN(nn.Module):
     def __init__(self, in_channels: int = 1, out_channels = 8):
         super(CNN, self).__init__()        
         self.conv1 = nn.Sequential(         
-            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=5, padding=3),                              
+            nn.Conv2d(in_channels=in_channels, out_channels=225, kernel_size=15, stride=15),                              
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=3), 
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Conv2d(in_channels=225, out_channels=625, kernel_size=1), 
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3), 
+            nn.MaxPool2d(kernel_size=(2,1)),
+            nn.Conv2d(in_channels=625, out_channels=1225, kernel_size=1), 
             nn.ReLU(), 
-            nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=1), 
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Conv2d(in_channels=1225, out_channels=2025, kernel_size=1), 
             nn.ReLU(),
         )
 
         self.fc1 = nn.Sequential(
-            nn.Linear(1152, out_channels)
+            nn.Linear(2*1*2025, out_channels)
         )
     def forward(self, x):
         x = self.conv1(x)
