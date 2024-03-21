143cb3d2c06adc360d02d76d345aee4e50cb5ea1
JIAN-MING <tonyvony0821@gmail.com>
2024-03-21 12:45:27 +0800

    add multicolor_shape_database background to black

--- load_data.py
+++ load_data.py
@@ -163,11 +163,19 @@ class MultiColorShapesDataset(Dataset):
             for name in files:
                 name_split = '_'.join(name.split('_')[:2])
                 label = self.label_to_num[name_split]
-
                 y_onehot = np.eye(9)[label]
                 y_onehot = torch.from_numpy(y_onehot)
                 label_dataset.append(y_onehot)
+
                 image = read_image(os.path.join(root, name))
+                image = image.permute(1,2,0)
+                black_color = torch.tensor([0.0, 0.0, 0.0])  # 黑色的 RGB 值
+                # 定义背景颜色和蓝色球颜色的阈值
+                background_threshold = 0.8  # 背景的阈值（每个通道大于这个值的像素被认为是背景）
+                # 找到背景和蓝色球的像素位置
+                background_indices = torch.all(image >= background_threshold, dim=-1)
+                image[background_indices] = black_color
+                image = image.permute(2,0,1)
                 image_dataset.append(image)
 
         return image_dataset, label_dataset
