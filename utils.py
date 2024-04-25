9ac380afeea86bdacdbc893f5c7e4075cb251da4
JIAN-MING <tonyvony0821@gmail.com>
2024-04-25 16:15:28 +0800

    add threshold in to utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
 from pathlib import Path
 import glob
 import re
+import torchvision
+
+__all__ = ['gray_transform']
+
+# 二值化
+class ThresholdTransform(object):
+        def __call__(self,x):
+            threshold = x.mean()
+            return (x>threshold).to(x.dtype)
+        
+gray_transform = torchvision.transforms.Compose([
+    torchvision.transforms.Grayscale(),
+    ThresholdTransform()
+])
 
 def increment_path(path, exist_ok=True, sep=''):
     # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
@@ -13,3 +27,5 @@ def increment_path(path, exist_ok=True, sep=''):
         i = [int(m.groups()[0]) for m in matches if m]  # indices
         n = max(i) + 1 if i else 2  # increment number
         return f"{path}{sep}{n}"  # update path
+    
+
