83b54df51b61883ea7aca86290ada692e143dce2
JIAN-MING <tonyvony0821@gmail.com>
2024-04-25 16:15:06 +0800

    update config_example to fit new SFMCNN

--- config_example.py
+++ config_example.py
@@ -6,9 +6,9 @@ import os
 
 project = "paper experiment"
 name = "SFMCNN"
-group = "1/25"
+group = "3/6"
 tags = ["SFMCNN", "Rewrite"]
-description = "1/25 測試 face 資料集"
+description = "3/6 測試 face 資料集， 15 * 15版本，input size 改為 (60,60)，無overlapping，後面加入(1,2) SFM layer 和一層 (2025,1225,1,1) 的 Conv layer"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 lr_scheduler = {
@@ -30,14 +30,21 @@ arch = {
     "args":{
         "in_channels": 1,
         "out_channels": 2,
-        "Conv2d_kernel": [(23,23), (1, 1), (1, 1), (1, 1), (1, 1)],
-        "SFM_filters": [(3, 3), (2, 1), (1, 2)],
-        "channels": [1, 100, 225, 625, 1225],
-        "strides": [1, 1, 1, 1, 1],
-        "paddings": [0, 0, 0, 0, 0],
-        "w_arr": [25.45, 12.45, 17.45, 27.45],
-        "percent": [0.5, 0.4, 0.3, 0.2, 0.2],
-        "fc_input": 60025,
+        "Conv2d_kernel": [(15, 15), (1, 1), (1, 1), (1, 1)],
+        "SFM_filters": [(1, 2), (2, 1), (1, 2)],
+        "channels": [1, 225, 625, 1225, 2025],
+        "strides": [15, 1, 1, 1],
+        "paddings": [0, 0, 0, 0],
+
+        "rbfs": ['triangle', 'triangle', 'triangle', 'triangle'],
+        "w_arr": [17.45, 17.45, 27.45, 37.45],
+        "percents": [0.5, 0.4, 0.3, 0.2],
+
+        # "rbfs": ['gauss', 'gauss', 'gauss', 'gauss'],
+        # "stds": [2, 2, 2, 2],
+        # "biass": [0.7, 0.7, 0.7, 0.7],
+
+        "fc_input": 2*1*2025,
         "device": device
     }
 }
@@ -63,10 +70,9 @@ config = {
     "save_dir": './runs/train/exp',
     "model": arch,
     "dataset": 'face_dataset',
-    "input_shape": (64, 64),
-    "rbf": "triangle",
+    "input_shape": (60, 60),
     "batch_size": 128,
-    "epoch" : 10,
+    "epoch" : 20,
     "lr" : 0.001,
     "lr_scheduler": lr_scheduler,
     "optimizer": optimizer,
