7e35ac7f25d6f61e0aaa35e4d0e596fbcc37c9ab
JIAN-MING <tonyvony0821@gmail.com>
2024-06-03 21:39:01 +0800

    fix Colored_MNIST problem

--- dataloader/Colored_MNIST.py
+++ dataloader/Colored_MNIST.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
 
 from torch.utils.data import Dataset
 from typing import Any, Callable, Optional, Tuple
@@ -20,28 +22,36 @@ class Colored_MNIST(Dataset):
         
         self.train = train
         self.augmentation = augmentation
-        self.data, self.targets = self._load_data()
-        self.targets = np.eye(30)[self.targets]
+        self._load_data()
+        
         
     def _load_data(self):
+        self.label_to_idx = {}
+        i = 0
+        for c in ['red', 'green', 'blue']:
+            for n in range(10):
+                self.label_to_idx[c+'_'+str(n)] = i
+                i+=1
+        
         self.data = np.load(f"{self.root}/Color_MNIST/{'Train' if self.train else 'Test'}_imgs.npy")
-        self.targets = np.load(f"{self.root}/Color_MNIST/{'Train' if self.train else 'Test'}_labels.npy")
-        label_to_idx = {}
-        for i in range(30):
-            for c in ['red', 'green', 'blue']:
-                for n in range(10):
-                    label_to_idx[c+'_'+n] = i
+        self.data = self.data.astype(np.float)
 
+        self.targets = np.load(f"{self.root}/Color_MNIST/{'Train' if self.train else 'Test'}_labels.npy")
+        self.targets = list(map(lambda x: np.eye(30)[self.label_to_idx[x]], self.targets))
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         img, target = self.data[index], self.targets[index]
 
         if self.transform is not None:
             img = self.transform(img)
+        if torch.max(img) > 1:
+            img /= 255
+        plt.imshow(img.permute(1,2,0))
+        plt.show()
 
         if self.target_transform is not None:
             target = self.target_transform(target)
-        
+
         return img, target
 
     def __len__(self) -> int:
