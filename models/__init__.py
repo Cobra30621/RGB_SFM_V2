b22150b811f011d2f69179530b851b5c67b5b4b5
JIAN-MING <tonyvony0821@gmail.com>
2024-06-13 01:04:30 +0800

    add GooLeNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
-from . import CNN, SFMCNN, RGB_SFMCNN, AlexNet, ResNet
+from . import CNN, SFMCNN, RGB_SFMCNN, AlexNet, ResNet, GoogLeNet
 
 __all__ = ['CNN', 
            'SFMCNN', 
            'AlexNet', 
            "RGB_SFMCNN",
            "SFMCNN_old",
-           'ResNet']
+           'ResNet',
+           'GoogLeNet']
